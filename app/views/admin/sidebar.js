a3d03095249e75b084a744ab569829f078cd132a
ssurerus <saulosurerus@hotmail.com>
2023-10-18 19:29:00 -0300

    Side bar v2

--- app/views/admin/sidebar.js
+++ app/views/admin/sidebar.js
@@ -1,11 +1,11 @@
 
 function openSidebar(){
     let contents = document.getElementsByClassName("sidebar-link-content");
-    
+    let sidebar_button = document.getElementById('sidebar-button');
     let links = document.getElementById("sidebar-links");
 
     links.classList.toggle('sidebar-links-active');
-
+    sidebar_button.classList.toggle('sidebar-button-active');
     for(i=0; i<3; i++){
        contents[i].classList.toggle('sidebar-link-content-active');
     }
