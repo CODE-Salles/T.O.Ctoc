efc6af899a8508ca3d683b4defcffdcf2f5eef2c
ssurerus <saulosurerus@hotmail.com>
2023-10-18 21:42:32 -0300

    Alterações feitas por felipe apenas codada por mim

--- app/views/site/crud usuários/main.js
+++ app/views/site/crud usuários/main.js
@@ -1,141 +1,49 @@
-'use strict'
+ var newUser = document.getElementById('cadastrarCliente');
+ var editUser = document.getElementById('edit');
+ var showUser = document.getElementById('visualizar');
+ var deleteUser = document.getElementById('delete');
 
-const openModal = () => document.getElementById('modal')
-    .classList.add('active')
+ var modalBox = document.getElementById('modal');
+ var modalClose = document.querySelectorAll('[id^="cancelar"]')
+ var modalNewUser = document.getElementById('modal-new-user');
+ var modalManageUser = document.getElementById('modal-manage-user');
+ var modalDeleteUser = document.getElementById('modal-delete-user');
 
-const closeModal = () => {
-    clearFields()
-    document.getElementById('modal').classList.remove('active')
+ newUser.addEventListener('click', () => {manageModal('newUser')});
+ editUser.addEventListener('click', () => {manageModal('showUser')});
+ showUser.addEventListener('click', () => {manageModal('showUser')});
+ deleteUser.addEventListener('click', () => {manageModal('deleteUser')});
+
+ modalClose.forEach(element => {
+    element.addEventListener('click', () => {
+        manageModal('close');
+        closeModals();
+    });
+ })
+
+
+function closeModals(){
+    let modalElements = document.querySelectorAll('[id^="modal-"]');
+
+    modalElements.forEach(element => {
+        element.classList = 'modal-content';
+      });
 }
 
-
-const getLocalStorage = () => JSON.parse(localStorage.getItem('db_client')) ?? []
-const setLocalStorage = (dbClient) => localStorage.setItem("db_client", JSON.stringify(dbClient))
-
-// CRUD - create read update delete
-const deleteClient = (index) => {
-    const dbClient = readClient()
-    dbClient.splice(index, 1)
-    setLocalStorage(dbClient)
-}
-
-const updateClient = (index, client) => {
-    const dbClient = readClient()
-    dbClient[index] = client
-    setLocalStorage(dbClient)
-}
-
-const readClient = () => getLocalStorage()
-
-const createClient = (client) => {
-    const dbClient = getLocalStorage()
-    dbClient.push (client)
-    setLocalStorage(dbClient)
-}
-
-const isValidFields = () => {
-    return document.getElementById('form').reportValidity()
-}
-
-//Interação com o layout
-
-const clearFields = () => {
-    const fields = document.querySelectorAll('.modal-field')
-    fields.forEach(field => field.value = "")
-    document.getElementById('nome').dataset.index = 'new'
-    document.querySelector(".modal-header>h2").textContent  = 'Novo Cliente'
-}
-
-const saveClient = () => {
-    if (isValidFields()) {
-        const client = {
-            nome: document.getElementById('nome').value,
-            email: document.getElementById('email').value,
-        }
-        const index = document.getElementById('nome').dataset.index
-        if (index == 'new') {
-            createClient(client)
-            updateTable()
-            closeModal()
-        } else {
-            updateClient(index, client)
-            updateTable()
-            closeModal()
-        }
+ function manageModal(typeModal){
+    modalBox.classList.toggle('active')
+    switch(typeModal){
+        case 'newUser':
+            modalNewUser.classList.toggle('modal-content-active');
+            break;
+        case 'showUser':
+            modalManageUser.classList.toggle('modal-content-active');
+            break;
+        case 'deleteUser':
+            modalDeleteUser.classList.toggle('modal-content-active');
+            break;
+        case 'close':
+            break;
+        default:
     }
-}
-
-const createRow = (client, index) => {
-    const newRow = document.createElement('tr')
-    newRow.innerHTML = `
-        <td>${client.nome}</td>
-        <td>${client.email}</td>
-        <td>
-            <button type="button" class="button yellow" id="visualizar-${index}" >Vizualizar</button>
-            <button type="button" class="button green" id="edit-${index}">Editar</button>
-            <button type="button" class="button red" id="delete-${index}" >Excluir</button>
-        </td>
-    `
-    document.querySelector('#tableClient>tbody').appendChild(newRow)
-}
-
-const clearTable = () => {
-    const rows = document.querySelectorAll('#tableClient>tbody tr')
-    rows.forEach(row => row.parentNode.removeChild(row))
-}
-
-const updateTable = () => {
-    const dbClient = readClient()
-    clearTable()
-    dbClient.forEach(createRow)
-}
-
-const fillFields = (client) => {
-    document.getElementById('nome').value = client.nome
-    document.getElementById('email').value = client.email
-    document.getElementById('nome').dataset.index = client.index
-}
-
-const editClient = (index) => {
-    const client = readClient()[index]
-    client.index = index
-    fillFields(client)
-    document.querySelector(".modal-header>h2").textContent  = `Editando ${client.nome}`
-    openModal()
-}
-
-const editDelete = (event) => {
-    if (event.target.type == 'button') {
-
-        const [action, index] = event.target.id.split('-')
-
-        if (action == 'edit') {
-            editClient(index)
-        } else {
-            const client = readClient()[index]
-            const response = confirm(`Deseja realmente excluir o usuário ${client.nome}?`)
-            if (response) {
-                deleteClient(index)
-                updateTable()
-            }
-        }
-    }
-}
-
-updateTable()
-
-// Eventos
-document.getElementById('cadastrarCliente')
-    .addEventListener('click', openModal)
-
-document.getElementById('modalClose')
-    .addEventListener('click', closeModal)
-
-document.getElementById('salvar')
-    .addEventListener('click', saveClient)
-
-document.querySelector('#tableClient>tbody')
-    .addEventListener('click', editDelete)
-
-document.getElementById('cancelar')
-    .addEventListener('click', closeModal)
+ }
